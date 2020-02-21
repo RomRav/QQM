37584ced06fb12b0f3e49a27370b3eaee9aadf75
Romain <romain.ravault@gmail.com>
2020-02-21 21:18:29 +0100

    "DAOpays selectAll+selectOne+insert (à finir)"

--- daos/PaysDAO.php
+++ daos/PaysDAO.php
@@ -5,9 +5,10 @@
  * Bibliothéque d'accées au données des pays
  * @author Romain
  * 20/02/2020
+ * Last update 21/02/2020
  * 
- * selectAll(): récupération de la liste de tous les pays
- * selectOne($id): récupération un pays
+ * selectAll($pdo): récupération de la liste de tous les pays
+ * selectOne($pdo, $id): récupération un pays
  * insert(): ajout d'un pays
  * update($id): modification d'un pays
  * delete($id): Supression d'un pays
@@ -21,22 +22,66 @@ class PaysDAO {
      * selectAll()
      * @author Romain Ravault
      * 20/02/202
-     * @return type
+     * @return Object Table
      */
-    public static function selectAll() {
+    public static function selectAll($pdo) {
         $listCountry = [];
         try {
             $requet = "SELECT * FROM country";
-            $pdo = Connexion::seConnecter("bd.ini");
             $raws = $pdo->query($requet);
+            $raws->setFetchMode(PDO::FETCH_ASSOC);
             while ($raw = $raws->fetch()) {
-                $country = new Pays($raw[0], $raw[1]);
+                $country = new Pays($raw['id_country'], $raw['country_name']);
                 array_push($listCountry, $country);
             }
         } catch (PDOException $ex) {
             echo 'ERREUR:' . $ex->getMessage();
         }
         return $listCountry;
+    }
+
+    /**
+     * selectOne()
+     * @author Romain Ravault
+     * 21/02/202
+     * @return type
+     */
+    public static function selectOne(pdo $pdo, int $idPays) {
+        try {
+            $requet = "SELECT * FROM country WHERE id_country = ? ;";
+            $stmt = $pdo->prepare($requet);
+            $stmt->bindParam(1, $idPays);
+            $stmt->execute();
+            $raw = $stmt->fetch(PDO::FETCH_ASSOC);
+            $country = new Pays($raw['id_country'], $raw['country_name']);
+        } catch (PDOException $ex) {
+            echo 'ERREUR:' . $ex->getMessage();
+        }
+        return $country;
+    }
+
+    /**
+     * insert()
+     * @author Romain Ravault
+     * 21/02/2020
+     * @param pdo $pdo
+     * @param string $newCountry
+     * @return type
+     */
+    public static function insert(pdo $pdo, string $newCountry) {
+        try {
+            $request = "INSERT INTO qqm.country(country_name) VALUE(?);";
+            $stmt = $pdo->prepare($request);
+            $stmt->bindParam(1, $newCountry);
+            $stmt->execute();
+            $stmt->debugDumpParams();
+            $insertVerif = $stmt->rowCount();
+            
+        } catch (PDOException $ex) {
+            echo $ex->getMessage();
+            $insertVerif = -1;
+        }
+        return $insertVerif;
     }
 
 }
--- daos/paysDAOTest.php
+++ daos/paysDAOTest.php
@@ -8,14 +8,41 @@
 
 require_once 'PaysDAO.php';
 require_once '../ett/Pays.php';
+require_once 'Connexion.php';
+
+//Connexion à la BD
+$pdo = Connexion::seConnecter("bd.ini");
+$pdo->beginTransaction();
 
 
 //Test de la selection de tous les pays
-$country = PaysDAO::selectAll();
-
+$country = PaysDAO::selectAll($pdo);
+echo '<hr><br>SELECT ALL<br><hr>';
 foreach ($country as $raw) {
     //var_dump($raw);
     //$enr = new Pays($raw[0], $raw[1]);
-    echo 'id pays: ' . $raw->getIdPays() . ' nom du pays: ' . $raw->getPays() . '.<br>';
+    echo 'id pays: ' . $raw->getIdPays() . '  //***\\ nom du pays: ' . $raw->getPays() . '.<br>';
 }
 
+//Test de la selection d'un pays
+echo '<hr><br>SELECT UNE PAYS PAR SON ID<br><hr>';
+$idPays = 1;
+$country = PaysDAO::selectOne($pdo, $idPays);
+echo 'id pays: ' . $country->getIdPays() . '  //***\\ nom du pays: ' . $country->getPays() . '.<br>';
+
+
+
+//Test de l'ajout d'un nouveau pays
+echo '<hr><br>AJOUTER UN PAYS<br><hr>';
+$newCountry = 'aa';
+
+$addCountry = PaysDAO::insert($pdo, $newCountry);
+if ($addCountry == 1) {
+    $pdo->commit();
+    echo $addCountry . ' pays à bien été enregisté.';
+} else {
+    echo 'Enregistrement du pays non réalisé.' . $addCountry;
+    $pdo->rollBack();
+}
+
+Connexion::seDeconnecter($pdo);
