2c03455826ad6445dbb7b616ad45d86ead9890d9
RomRav <romain.ravault@gmail.com>
2020-02-29 10:48:21 +0100

    Insertion de la recette + récupération de l'identifiant de la recette.

--- ctrl/newRecipeCtrl.php
+++ ctrl/newRecipeCtrl.php
@@ -11,15 +11,17 @@ require_once '../daos/SeasonDAO.php';
 require_once '../daos/PositionDAO.php';
 require_once '../daos/PaysDAO.php';
 require_once '../daos/IngredientDAO.php';
+require_once '../daos/RecipeDAO.php';
 require_once '../ett/Type.php';
 require_once '../ett/newRecipe.php';
 
 //Connexion 
 $pdo = Connexion::seConnecter("../daos/bd.ini");
+$pdo->beginTransaction();
 
 //Récupération de la saisie du formulaire de création des recettes
 $titre = filter_input(INPUT_POST, "titre", FILTER_SANITIZE_SPECIAL_CHARS);
-$season = filter_input_array(INPUT_POST, "season[]");
+$season = filter_input_array(INPUT_POST, "season");
 $position = filter_input(INPUT_POST, "position");
 $contenue = filter_input(INPUT_POST, "contenue", FILTER_SANITIZE_SPECIAL_CHARS);
 $contenuRecette = filter_input(INPUT_POST, "contenuRecette");
@@ -28,13 +30,22 @@ $country = filter_input(INPUT_POST, "country");
 $message = "";
 
 
+
 //Contrôle et enregistrement du contenu saisie dans le formulaire d'ajout d'une recette
-//if (!isset($titre, $season, $position, $position, $contenuRecette, $ingredient, $country)) {
-//    $message = "L'un des champs n'a pas été rempli ou une caractéristique n'a pas été sélectionner.";
-//} else {
-//    $newRecipe = new NewRecipe($titre, $season, $position, $contenue, $contenuRecette, $ingredient, $country);
-//  
-//}
+if (!isset($titre, $season, $position, $position, $contenuRecette, $ingredient, $country)) {
+    $message = "L'un des champs n'a pas été rempli ou une caractéristique n'a pas été sélectionner.";
+} else {
+    $newRecipe = new NewRecipe($titre, $season, $position, $contenue, $contenuRecette, $ingredient, $country);
+    $recRecipe = RecipeDAO::insert($pdo, $titre, $contenuRecette, 1, 2);
+    if ($recRecipe == 1) {
+         $newRecipeId = $pdo->lastInsertId();
+        $pdo->commit();
+        echo $recRecipe. " recette bien enregistré";
+        echo $newRecipeId." est l'id de la nouvelle recette";
+    } else {
+        $pdo->rollBack();
+    }
+}
 //Récupération de la liste des type de plats
 $selectType = "";
 $typesTable = TypeDAO::selectAll($pdo);
